4be1c5366196725814f11256a93c367fa8b1cbba
neha.sharma <neha.sharma@icts.res.in>
2024-10-04 16:43:41 +0530

    Added new function

--- lalsimulation/lib/LALSimIMRPhenomTHM.h
+++ lalsimulation/lib/LALSimIMRPhenomTHM.h
@@ -59,6 +59,22 @@ int LALSimIMRPhenomTHM_Modes(
   UINT4 only22              /**< Internal flag for mode array check */
   );
 
+int LALSimIMRPhenomTHM_Modes_neha(
+  SphHarmTimeSeries **hlms,   /**< [out] Time domain modes */
+  REAL8 m1_SI,                /**< Mass of companion 1 (kg) */
+  REAL8 m2_SI,                /**< Mass of companion 2 (kg) */
+  REAL8 chi1L,                /**< Dimensionless aligned spin of companion 1 */
+  REAL8 chi2L,                /**< Dimensionless aligned spin of companion 2 */
+  REAL8 distance,             /**< Luminosity distance (m) */
+  REAL8 deltaT,               /**< sampling interval (s) */
+  REAL8 fmin,               /**< starting GW frequency (Hz) */
+  REAL8 fRef,               /**< reference GW frequency (Hz) */
+  REAL8 phiRef,               /**< reference orbital phase (rad) */
+  REAL8Sequence *TimeArray,
+  LALDict *lalParams,            /**< LAL dictionary containing accessory parameters */
+  UINT4 only22              /** Internal flag for mode array check */
+  );
+
 /* Function to adapt user requested mode list to modes included in the model */
 LALDict *IMRPhenomTHM_setup_mode_array(LALDict *lalParams);
 int check_input_mode_array_THM(LALDict *lalParams);
